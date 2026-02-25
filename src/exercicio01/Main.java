6e9c24eb1f54a43e2d9bf3f6936dd89942139bc2
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-25 11:38:09 -0300

    Exercicio 1 finalizado

---
+++ src/exercicio01/Main.java
@@ -0,0 +1,31 @@
+package exercicio01;
+
+import java.text.DecimalFormat;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Conta conta = new Conta();
+        Random rd = new Random();
+        DecimalFormat df = new DecimalFormat("#,##0.00");
+        double valor;
+
+        System.out.println("Correntista --> ");
+        conta.correntista = sc.next();
+        conta.numero = rd.nextInt(1000,9999);
+
+        System.out.println("Numero da conta " + conta.numero);
+        System.out.println("saldo inicial R$ " + df.format(conta.saldo));
+        System.out.println("Valor para depósito R$ ");
+        valor = sc.nextDouble();
+        conta.depositar(valor);
+        System.out.println("Saldo atual R$ " + df.format(conta.saldo));
+        System.out.println("Valor para saque R$ ");
+        valor = sc.nextDouble();
+        conta.sacar(valor);
+        System.out.println("Saldo atual R$ " + df.format(conta.saldo));
+    }
+}
